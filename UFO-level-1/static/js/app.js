9621145ff12250a96bf4bf9d87f86aeb585b1606
Romero-ML <marialuzromero@Marias-MacBook-Pro.local>
2019-12-09 02:36:06 -0500

    final files

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,23 +1,16 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-//select the filter type
-// var fiterTypeValue = d3.select("#filter-type");
-// var filterTypeValue = d3.select("#filter-type-value");
 
-//select the submit button
-//var submit = d3.select("#filter-btn");
 
 // get a reference to the table body
 var tbody = d3.select("tbody");
 
 
-// console.log the aliens data from data.js
-//console.log(tableData);
+
 // Shows all the data in the file to the webpage
 autopopulate(tableData);
-//autoPopulate(tableData);
+
 
 
 
@@ -59,80 +52,3 @@ console.log(newData);
 autopopulate(newData);
 }
 
-
-
-//funtion to link slection of an item from dropdown
-/* filterType=d3.select("#filter-btn")
-filterType.on("change", function() {
-    var filterValue = filterType.property("value");
-    d3.select("#datetime").node().value ='';
-    // stetting placeholder values for inout text
-    switch (filterValue) {
-        case 'date/time':
-            placeHolder = '1/1/2011';
-        break
-        case 'city': 
-            placeHolder = 'city';
-        break
-        case 'state':
-            placeHolder = 'state';
-        break
-        case 'country':
-            placeHolder = 'country';
-        break
-        case 'shape':
-            placeHolder = 'shape';
-        break
-        default: 
-            placeHolder = '';
-
-    }
-d3.select("input").attr('placeHolder', placeHolder);
-d3.select('label')
-    .attr("for", filterValue)
-    .text('Enter a value for ${filterValue.toUpperCase()}');
-});
-d3.selectAll('form').on('submit', () => d3.event.preventDefault())
-
-// function to link on clicking button
-submit.on('click', function(){
-
-    //prevent the page from refreshing
-    d3.event.preventDefault();
-
-    // clear previous data
-    tbody.html("");
-
-    //get the data entered in the text box
-    var inputElement = d3.select("#datetime");
-    console.log(inputElement)
-    var inputValue = inputElement.property("value");
-    console.log(inputValue)
-
-    if (inputValue == '') {
-        alert("Please enter value");
-        document.getElementById ("#datetime").focus();
-        autoPopulate(tableData);
-    }
-
-    // filter the data on the input value
-    var typeVal = d3.select("label").attr("for");
-
-    var filterData = tableData.filter(aliens => aliens[typeVal] === inputValue);
-    if (filterData.length == 0) {
-        alert("Opps..No UFO's Found");
-        d3.select("#datetime").node().value = '';
-        autoPopulate(tableData);
-    }
-    console.log(filterData);
-
-    //display the data for the selection
-    //filterData.forEach((alients) => {
-        //var row = tbody.append("tr");
-        Object.entries(filterData).forEach(([key, values]) => {
-            var row = tbody.append("tr");
-            var cell = row.append("td");
-            cell.text(values);
-            console.log(values)
-        });
-    }); */
